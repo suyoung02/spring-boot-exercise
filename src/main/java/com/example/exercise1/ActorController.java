1cbf51726aa241c4dcf6a5856f4713ddde6cb7bb
Vy <vy.do@pharmapoc.com>
2023-10-10 17:03:57 +0700

    chore: fix typo

--- src/main/java/com/example/exercise1/ActorController.java
+++ src/main/java/com/example/exercise1/ActorController.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/api/actor")
+@RequestMapping(value = "/api/actors")
 public class ActorController {
     private ActorServices actorServices;
 
