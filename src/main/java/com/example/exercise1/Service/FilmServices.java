f4bf6105f009d2be0b4c0a6bf99e388cee8319dd
ThaiLV <luongvuthai1st@example.com>
2023-10-23 20:58:26 +0700

    add 3 api film

--- src/main/java/com/example/exercise1/Service/FilmServices.java
+++ src/main/java/com/example/exercise1/Service/FilmServices.java
@@ -3,8 +3,11 @@ package com.example.exercise1.Service;
 import java.sql.Timestamp;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
+import com.example.exercise1.Exception.AppException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import com.example.exercise1.Model.Film;
@@ -26,5 +29,22 @@ public class FilmServices {
     public Film createNewFilm(Film film) {
         film.setLastUpdate(new Timestamp((new Date()).getTime()));
         return filmRepository.save(film);
+    }
+    public Film findById(Integer theId){
+        Optional<Film> res=filmRepository.findById(theId);
+        Film theFilm=null;
+        if(res.isPresent()){
+            theFilm=res.get();
+        }else{
+            throw new AppException(HttpStatus.NOT_FOUND.value(), HttpStatus.NOT_FOUND, "Film is Not Found");
+        }
+        return theFilm;
+    }
+    public Film save(Film theFilm){
+        theFilm.setLastUpdate(new Timestamp((new Date()).getTime()));
+        return filmRepository.save(theFilm);
+    }
+    public void deleteById(Integer theId){
+        filmRepository.deleteById(theId);
     }
 }
