c35821d7f07df733c54bb6806dc87273fd30b4d5
HuyDeveloper <vinhhuy16072002@gmail.com>
2023-10-31 12:28:28 +0700

    Search logs by date and level

--- src/main/java/com/example/exercise1/Controller/LoggerController.java
+++ src/main/java/com/example/exercise1/Controller/LoggerController.java
@@ -8,16 +8,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 
 
 @RestController
@@ -25,14 +21,14 @@ import java.util.Objects;
 public class LoggerController {
 
     @GetMapping("")
-    public ResponseEntity<List<LogEntry>> getLogs(@RequestParam(value = "level") String level, @RequestParam(value = "content") String message) {
+    public ResponseEntity<List<LogEntry>> getLogs(@RequestParam(value = "level") String level, @RequestParam(value = "date") String date) {
         List<LogEntry> list = new ArrayList<>();
         String folderPath = "C:\\Users\\vinhh\\Source_C\\logs";
 
         try {
             Files.walk(Paths.get(folderPath))
                     .filter(Files::isRegularFile)
-                    .forEach(filePath -> readLogFile(filePath, list, level, message));
+                    .forEach(filePath -> readLogFile(filePath, list, level, date));
         } catch (IOException e) {
             System.err.println("Error: " + e.getMessage());
         }
@@ -40,15 +36,30 @@ public class LoggerController {
         return ResponseEntity.ok(list);
     }
 
-    private void readLogFile(Path filePath, List<LogEntry> list, String level, String message) {
+    private void readLogFile(Path filePath, List<LogEntry> list, String level, String date) {
         try (BufferedReader reader = Files.newBufferedReader(filePath)) {
             String strLine;
             while ((strLine = reader.readLine()) != null) {
                 String[] words = strLine.split("\\s", 5);
                 LogEntry entry = new LogEntry(words[0], words[1], words[4], words[3]);
-                if(level.equals(entry.getLevel())){
-                    list.add(entry);
+                if(level.isEmpty() && !date.isEmpty()){
+                    String[] time = entry.getTime().split(":", 2);
+                    if(date.equals(time[0])){
+                        list.add(entry);
+                    }
                 }
+                else if(date.isEmpty() && !level.isEmpty()){
+                    if(level.equals(entry.getLevel())){
+                        list.add(entry);
+                    }
+                }
+                else if(!date.isEmpty() && !level.isEmpty()){
+                    String[] time = entry.getTime().split(":", 2);
+                    if(level.equals(entry.getLevel()) && date.equals(time[0])){
+                        list.add(entry);
+                    }
+                }
+
             }
         } catch (IOException e) {
             System.err.println("Error reading file: " + filePath + "\n" + e.getMessage());
